bfae8df55e1603752de209354a9e40e71619b871
kxknet <serviceseo@gmail.com>
2021-01-03 15:28:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "This VNC installer by @kxkwork"
-sudo apt-get install -y xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
-sudo apt-get install tigervnc-standalone-server tigervnc-common
+apt-get install -y xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
+apt-get install tigervnc-standalone-server tigervnc-common
 echo "Install completed"
 echo "need configure, answer questions and set passwords"
 vncserver -localhost no
