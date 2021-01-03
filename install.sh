2381a935ec82ab6598afd9235eb539582808933a
kxknet <serviceseo@gmail.com>
2021-01-03 15:43:11 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 echo "This VNC installer by @kxkwork"
 cd /root/
 apt-get install -y xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
+echo "screen Configuring gdm select 1"
 apt-get install tigervnc-standalone-server tigervnc-common
 echo "Install completed"
 echo "need configure, answer questions and set passwords"
@@ -9,4 +10,5 @@ vncserver -localhost no
 wget https://raw.githubusercontent.com/kxknet/vnc/main/xstartup
 mv /root/xstartup ~/.vnc/xstartup
 chmod 755 ~/.vnc/xstartup
+echo "need select 2 down"
 update-alternatives --config x-terminal-emulator
